aa614a3db1df71b7e3e50083e5af86870c68c57c
mt_tahmid <md.tasin.tahmid1998@gmail.com>
2023-04-08 14:35:16 +0600

    nineth commit

--- client/src/pages/auth.js
+++ client/src/pages/auth.js
@@ -1,3 +1,83 @@
+import { useState } from "react";
+import axios from "axios";
+
 export const Auth = () => {
-  return <h1>Authpage...</h1>
+  return (
+    <div className="auth">
+      <Login/>
+      <Register/>
+    </div>
+  )
+}
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  return <Form 
+    username = {username}
+    setUsername ={ setUsername}
+    password = {password}
+    setPassword ={ setPassword }
+    label = "Login"
+  />
+}
+
+const Register = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onSubmit = async(event) => {
+    event.preventDefault() ;
+
+    try {
+      await axios.post('http://localhost:5000/auth/register',{
+        username,
+        password,
+      });
+
+      alert('Registration successfull.');
+
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  return <Form 
+    username = {username}
+    setUsername ={ setUsername}
+    password = {password}
+    setPassword ={ setPassword }
+    label = "Register"
+    onSubmit = {onSubmit}
+  />
+}
+
+const Form = ({username, password, setPassword, setUsername, label, onSubmit}) => {
+  return (
+
+    <div className="auth-container">
+      <form onSubmit={onSubmit}>
+        <h2>{label}</h2>
+
+        <div className="form-group">
+          <label htmlFor="username"> Username: </label>
+          <input type="text" 
+          id="username" 
+          value={username}
+          onChange={(e)=> setUsername(e.target.value)}/>
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="password"> Password: </label>
+          <input type="password" 
+          id="password" 
+          value={password}
+          onChange={(e)=>setPassword(e.target.value)}/>
+        </div>
+        <div>
+          <input type="submit" value={label} />
+        </div>
+      </form>
+    </div>
+  )
 }
